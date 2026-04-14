aeae9f7484a845f8d50ea1d31e7849b037d7e07f
simo-asl <aslaouisimo1@gmail.com>
2026-04-14 18:35:15 +0100

    Fixing Norminette

--- coders/time_utils.c
+++ coders/time_utils.c
@@ -6,19 +6,53 @@
 /*   By: mel-asla <mel-asla@student.1337.ma>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/07 09:55:18 by mel-asla          #+#    #+#             */
-/*   Updated: 2026/04/12 08:23:32 by mel-asla         ###   ########.fr       */
+/*   Updated: 2026/04/14 18:33:31 by mel-asla         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "codexion.h"
 
-long	get_timestamp_ms(void)
+static long	get_raw_timestamp_ms(void)
 {
 	struct timeval	tv;
 
 	if (gettimeofday(&tv, NULL) != 0)
 		return (0);
 	return ((tv.tv_sec * 1000L) + (tv.tv_usec / 1000L));
+}
+
+static pthread_mutex_t	*get_time_mutex(void)
+{
+	static pthread_mutex_t	time_mutex = PTHREAD_MUTEX_INITIALIZER;
+
+	return (&time_mutex);
+}
+
+long	get_timestamp_ms(void)
+{
+	static long			state[3];
+	long				raw_ms;
+	long				delta;
+
+	raw_ms = get_raw_timestamp_ms();
+	pthread_mutex_lock(get_time_mutex());
+	if (!state[0])
+	{
+		state[0] = 1;
+		state[1] = raw_ms;
+		state[2] = raw_ms;
+		pthread_mutex_unlock(get_time_mutex());
+		return (raw_ms);
+	}
+	delta = raw_ms - state[1];
+	if (delta < 0)
+		delta = 0;
+	if (delta > 1000)
+		delta = 1;
+	state[2] += delta;
+	state[1] = raw_ms;
+	pthread_mutex_unlock(get_time_mutex());
+	return (state[2]);
 }
 
 long	get_elapsed_time_ms(long start_time)
