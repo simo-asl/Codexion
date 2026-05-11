d0426e4eaa1143d45ec414b38b64624e7d690e1a
Mohammed El Aslaouy <aslaouisimo1@gmail.com>
2026-05-12 00:47:02 +0100

    Simulaion ends

--- coders/simulation.c
+++ coders/simulation.c
@@ -5,8 +5,8 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: mel-asla <mel-asla@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2026/04/11 12:26:14 by mel-asla          #+#    #+#             */
-/*   Updated: 2026/04/18 14:17:35 by mel-asla         ###   ########.fr       */
+/*   Created: 2026/04/14 12:11:36 by mel-asla          #+#    #+#             */
+/*   Updated: 2026/05/02 17:22:01 by mel-asla         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -21,9 +21,20 @@ static int	start_coder_threads(t_table *table)
 	{
 		if (pthread_create(&table->coders[i].thread, NULL, coder_routine,
 				&table->coders[i]) != 0)
+		{
+			set_simulation_end(table, true);
+			pthread_mutex_lock(&table->start_mutex);
+			table->start_released = true;
+			pthread_cond_broadcast(&table->start_cond);
+			pthread_mutex_unlock(&table->start_mutex);
+			while (--i >= 0)
+				pthread_join(table->coders[i].thread, NULL);
 			return (1);
+		}
 		i++;
 	}
+	if (table->coder_count != i)
+		return (1);
 	return (0);
 }
 
@@ -43,6 +54,10 @@ int	start_simulation(t_table *table)
 {
 	if (start_coder_threads(table) != 0)
 		return (1);
+	pthread_mutex_lock(&table->start_mutex);
+	table->start_released = true;
+	pthread_cond_broadcast(&table->start_cond);
+	pthread_mutex_unlock(&table->start_mutex);
 	if (pthread_create(&table->monitor_thread, NULL,
 			monitor_routine, table) != 0)
 	{
