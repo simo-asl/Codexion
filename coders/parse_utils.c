6d8b96b552a2145a7ae702163687e4dc730770a1
Mohammed El Aslaouy <aslaouisimo1@gmail.com>
2026-05-12 01:10:00 +0100

    Utils

--- coders/parse_utils.c
+++ coders/parse_utils.c
@@ -6,7 +6,7 @@
 /*   By: mel-asla <mel-asla@student.42.fr>          +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2026/04/14 11:33:09 by mel-asla          #+#    #+#             */
-/*   Updated: 2026/04/25 00:54:34 by mel-asla         ###   ########.fr       */
+/*   Updated: 2026/05/12 00:57:31 by mel-asla         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,6 +19,8 @@ int	is_number(const char *str)
 	if (!str || !str[0])
 		return (0);
 	i = 0;
+	if (str[i] == '+')
+		i++;
 	while (str[i])
 	{
 		if (str[i] < '0' || str[i] > '9')
@@ -37,6 +39,10 @@ long	ft_atol(const char *str)
 		return (-1);
 	result = 0;
 	i = 0;
+	if (str[i] == '+')
+		i++;
+	else if (str[i] == '-')
+		return (-1);
 	while (str[i])
 	{
 		result = result * 10 + (str[i] - '0');
@@ -55,17 +61,6 @@ int	parse_positive_int(const char *str, int *value)
 	if (result <= 0 || result > INT_MAX)
 		return (1);
 	*value = (int)result;
-	return (0);
-}
-
-int	parse_positive_long(const char *str, long *value)
-{
-	long	result;
-
-	result = ft_atol(str);
-	if (result <= 0)
-		return (1);
-	*value = result;
 	return (0);
 }
 
